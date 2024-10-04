636a12ede229a3463993f9bdf8795d7f87b0d607
Langlois Cyril <cyril.langlois@insa-lyon.fr>
2024-10-04 14:29:39 +0200

    setup.py creation for editable library

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='inichord',
+      version='1.0 version dev',
+      description='inichord_dev',
+      packages=['inichord'],
+     )
