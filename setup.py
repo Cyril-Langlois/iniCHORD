f2beadbf54e42fc299f529488bc542166a6aa061
Langlois Cyril <cyril.langlois@insa-lyon.fr>
2024-10-09 17:29:12 +0200

    autre essai

--- setup.py
+++ setup.py
@@ -2,11 +2,19 @@
 
 from distutils.core import setup
 
+def get_data_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+			if filename[-1] =="i":
+				paths.append(os.path.join(path, filename))
+    return paths
+
 setup(name='inichord',
       version='1.0',
       description='inichord_dev',
       packages=['inichord'],
 	  data_files=[('Lib/site-packages/inichord', ['inichord/config.txt']),
-				  ('Lib/site-packages/inichord', ['inichord/*.ui']),
+				  ('Lib/site-packages/inichord', get_data_files(inichord)),
      ],
      )
